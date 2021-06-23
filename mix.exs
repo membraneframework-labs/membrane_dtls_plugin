9c587bde4c1b9e0154b0f98531929f049994985b
Michał Śledź <michal.sledz@swmansion.com>
2021-06-23 14:09:39 +0200

    Fix tests. Update deps

--- mix.exs
+++ mix.exs
@@ -38,8 +38,8 @@ defmodule Membrane.DTLS.Plugin.Mixfile do
   defp deps do
     [
       {:membrane_core, "~> 0.7.0"},
-      {:membrane_ice_plugin, github: "membraneframework/membrane_ice_plugin"},
-      {:ex_dtls, github: "membraneframework/ex_dtls"},
+      {:membrane_ice_plugin, "~> 0.5.0"},
+      {:ex_dtls, "~> 0.5.0"},
       {:ex_doc, "~> 0.23", only: :dev, runtime: false},
       {:dialyxir, "~> 1.0.0", only: :dev, runtime: false},
       {:credo, "~> 1.5", only: :dev, runtime: false}
