6109ec1715c69730baa83ee064f534ebb291158c
Michał Śledź <sledzson08@gmail.com>
2021-12-29 11:21:24 +0100

    Update deps. Bump version to 0.7.0.

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Membrane.DTLS.Plugin.Mixfile do
   use Mix.Project
 
-  @version "0.6.0"
+  @version "0.7.0"
   @github_url "https://github.com/membraneframework/membrane_dtls_plugin"
 
   def project do
@@ -37,9 +37,9 @@ defmodule Membrane.DTLS.Plugin.Mixfile do
 
   defp deps do
     [
-      {:membrane_core, "~> 0.7.0"},
-      {:membrane_ice_plugin, "~> 0.7.0"},
-      {:ex_dtls, "~> 0.7.0"},
+      {:membrane_core, "~> 0.8.0"},
+      {:membrane_ice_plugin, "~> 0.8.0"},
+      {:ex_dtls, "~> 0.8.0"},
       {:ex_doc, "~> 0.23", only: :dev, runtime: false},
       {:dialyxir, "~> 1.0.0", only: :dev, runtime: false},
       {:credo, "~> 1.5", only: :dev, runtime: false}
