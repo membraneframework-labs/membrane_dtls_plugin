a46e91627eaae0fab0e151930be6ec01b579fe73
feliks.pobiedzinski@swmansion.com <feliks.pobiedzinski@swmansion.com>
2022-01-19 11:45:53 +0100

    Update dependency to membrane_libnice_plugin

--- mix.exs
+++ mix.exs
@@ -38,8 +38,7 @@ defmodule Membrane.DTLS.Plugin.Mixfile do
   defp deps do
     [
       {:membrane_core, "~> 0.8.0"},
-      {:membrane_libnice_plugin,
-       github: "membraneframework/membrane_ice_plugin", branch: "remove-fake-turn-dependency"},
+      {:membrane_libnice_plugin, github: "membraneframework/membrane_ice_plugin"},
       {:ex_dtls, "~> 0.8.0"},
       {:ex_doc, "~> 0.23", only: :dev, runtime: false},
       {:dialyxir, "~> 1.0.0", only: :dev, runtime: false},
