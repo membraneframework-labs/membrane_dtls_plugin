ebbbd8983731df6424463f240b26740534743ce7
feliks.pobiedzinski@swmansion.com <feliks.pobiedzinski@swmansion.com>
2022-03-28 14:40:45 +0200

    Update dependency to membrane_libnice_plugin

--- mix.exs
+++ mix.exs
@@ -38,7 +38,7 @@ defmodule Membrane.DTLS.Plugin.Mixfile do
   defp deps do
     [
       {:membrane_core, "~> 0.8.0"},
-      {:membrane_libnice_plugin, github: "membraneframework/membrane_libnice_plugin"},
+      {:membrane_libnice_plugin, "~> 0.1.0"},
       {:ex_dtls, "~> 0.8.0"},
       {:ex_doc, "~> 0.23", only: :dev, runtime: false},
       {:dialyxir, "~> 1.0.0", only: :dev, runtime: false},
